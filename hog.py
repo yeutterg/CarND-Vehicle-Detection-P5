945edb9570c6ebd971f487a7c0bd94557f5df86b
Greg Yeutter <greg@Gregs-MacBook-Pro.local>
2017-05-17 07:50:39 -0400

    svc classifier working

--- hog.py
+++ hog.py
@@ -6,7 +6,9 @@ import cv2
 from skimage.feature import hog
 from img_processing import convert_color
 from features import bin_spatial, color_hist
+from sklearn.utils import shuffle
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
 
 # dist_pickle = pickle.load(open("svc_pickle.p", "rb"))
 # svc = dist_pickle["svc"]
@@ -209,7 +211,7 @@ def scale_features(cars_train_feat, cars_test_feat, cars_valid_feat, noncars_tra
     """
     # Stack the features into a matrix
     stack = np.vstack((cars_train_feat, cars_valid_feat, cars_test_feat, noncars_train_feat,
-                       noncars_valid_feat, noncars_test_feat))
+                       noncars_valid_feat, noncars_test_feat)).astype(np.float64)
 
     # Fit a StandardScaler based on the shape of the stack
     scaler = StandardScaler().fit(stack)
@@ -219,8 +221,7 @@ def scale_features(cars_train_feat, cars_test_feat, cars_valid_feat, noncars_tra
 
 
 def generate_train_valid_test(cars_train_feat, cars_test_feat, cars_valid_feat,
-                              noncars_train_feat, noncars_test_feat, noncars_valid_feat,
-                              random_state=42):
+                              noncars_train_feat, noncars_test_feat, noncars_valid_feat):
     """
     Generates X and y datasets for training, validation, and testing
 
@@ -231,7 +232,6 @@ def generate_train_valid_test(cars_train_feat, cars_test_feat, cars_valid_feat,
     :param noncars_train_feat:
     :param noncars_test_feat:
     :param noncars_valid_feat:
-    :param random_state:
     :return:
     """
     # Get the length of each dataset
@@ -270,108 +270,139 @@ def generate_train_valid_test(cars_train_feat, cars_test_feat, cars_valid_feat,
     y_test = np.hstack((np.ones(c_test_len), np.zeros(n_test_len)))
 
     # Shuffle the values
-    X_train, y_train = shuffle(X_train, y_train, random_state)
-    X_valid, y_valid = shuffle(X_valid, y_valid, random_state)
-    X_test, y_test = shuffle(X_test, y_test, random_state)
+    X_train, y_train = shuffle(X_train, y_train, random_state=42)
+    X_valid, y_valid = shuffle(X_valid, y_valid, random_state=42)
+    X_test, y_test = shuffle(X_test, y_test, random_state=42)
 
     return X_train, X_valid, X_test, y_train, y_valid, y_test
 
 
+def sv_classifier(X_train, X_valid, X_test, y_train, y_valid, y_test):
+    """
+    Runs the support vector classifier on X_ and y_ datasets
+
+    :param X_train:
+    :param X_valid:
+    :param X_test:
+    :param y_train:
+    :param y_valid:
+    :param y_test:
+    :return:
+    """
+    svc = LinearSVC()
+    svc.fit(X_train, y_train)
+    valid_acc = svc.score(X_valid, y_valid)
+    test_acc = svc.score(X_test, y_test)
+    return svc, valid_acc, test_acc
 
 
-cars_train_feat, cars_test_feat, cars_valid_feat, noncars_train_feat, noncars_test_feat, \
-noncars_valid_feat = get_features_all_datasets()
 
-X_train, X_valid, X_test, y_train, y_valid, y_test = generate_train_valid_test(cars_train_feat, cars_test_feat,
-                                                                               cars_valid_feat, noncars_train_feat,
-                                                                               noncars_test_feat, noncars_valid_feat)
 
+def apply_hog():
+    """
+
+    :return:
+    """
+    cars_train_feat, cars_test_feat, cars_valid_feat, noncars_train_feat, noncars_test_feat, noncars_valid_feat = get_features_all_datasets()
+
+    X_train, X_valid, X_test, y_train, y_valid, y_test = generate_train_valid_test(cars_train_feat, cars_test_feat,
+                                                                                   cars_valid_feat, noncars_train_feat,
+                                                                                   noncars_test_feat,
+                                                                                   noncars_valid_feat)
+
+    svc, test_acc, valid_acc = sv_classifier(X_train, X_valid, X_test, y_train, y_valid, y_test)
+
+    print('Validation Accuracy:', valid_acc)
+    print('Test Accuracy:', test_acc)
+
+
+apply_hog()
 
 
 ### old ###
 
 
 
-# Define a single function that can extract features using hog sub-sampling and make predictions
-def hog_find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins):
-    draw_img = np.copy(img)
-    img = img.astype(np.float32) / 255
-
-    img_tosearch = img[ystart:ystop, :, :]
-    ctrans_tosearch = convert_color(img_tosearch, conv='RGB2YCrCb')
-    if scale != 1:
-        imshape = ctrans_tosearch.shape
-        ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1] / scale), np.int(imshape[0] / scale)))
-
-    ch1 = ctrans_tosearch[:, :, 0]
-    ch2 = ctrans_tosearch[:, :, 1]
-    ch3 = ctrans_tosearch[:, :, 2]
-
-    # Define blocks and steps as above
-    nxblocks = (ch1.shape[1] // pix_per_cell) - cell_per_block + 1
-    nyblocks = (ch1.shape[0] // pix_per_cell) - cell_per_block + 1
-    nfeat_per_block = orient * cell_per_block ** 2
-
-    # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
-    window = 64
-    nblocks_per_window = (window // pix_per_cell) - cell_per_block + 1
-    cells_per_step = 2  # Instead of overlap, define how many cells to step
-    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
-    nysteps = (nyblocks - nblocks_per_window) // cells_per_step
-
-    # Compute individual channel HOG features for the entire image
-    hog1 = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
-    hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
-    hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
-
-    for xb in range(nxsteps):
-        for yb in range(nysteps):
-            ypos = yb * cells_per_step
-            xpos = xb * cells_per_step
-            # Extract HOG for this patch
-            hog_feat1 = hog1[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
-            hog_feat2 = hog2[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
-            hog_feat3 = hog3[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
-            hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3))
-
-            xleft = xpos * pix_per_cell
-            ytop = ypos * pix_per_cell
-
-            # Extract the image patch
-            subimg = cv2.resize(ctrans_tosearch[ytop:ytop + window, xleft:xleft + window], (64, 64))
-
-            # Get color features
-            spatial_features = bin_spatial(subimg, size=spatial_size)
-            hist_features = color_hist(subimg, nbins=hist_bins)
-
-            # Scale features and make a prediction
-            test_features = X_scaler.transform(
-                np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))
-            # test_features = X_scaler.transform(np.hstack((shape_feat, hist_feat)).reshape(1, -1))
-            test_prediction = svc.predict(test_features)
-
-            if test_prediction == 1:
-                xbox_left = np.int(xleft * scale)
-                ytop_draw = np.int(ytop * scale)
-                win_draw = np.int(window * scale)
-                cv2.rectangle(draw_img, (xbox_left, ytop_draw + ystart),
-                              (xbox_left + win_draw, ytop_draw + win_draw + ystart), (0, 0, 255), 6)
-
-    return draw_img
-
-
-def apply_hog(img):
-    """
-
-    :param img: The image
-    :return:
-    """
-    ystart = 400
-    ystop = 656
-    scale = 1.5
-
-    out_img = hog_find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size,
-                            hist_bins)
+# # Define a single function that can extract features using hog sub-sampling and make predictions
+# def hog_find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins):
+#     draw_img = np.copy(img)
+#     img = img.astype(np.float32) / 255
+#
+#     img_tosearch = img[ystart:ystop, :, :]
+#     ctrans_tosearch = convert_color(img_tosearch, conv='RGB2YCrCb')
+#     if scale != 1:
+#         imshape = ctrans_tosearch.shape
+#         ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1] / scale), np.int(imshape[0] / scale)))
+#
+#     ch1 = ctrans_tosearch[:, :, 0]
+#     ch2 = ctrans_tosearch[:, :, 1]
+#     ch3 = ctrans_tosearch[:, :, 2]
+#
+#     # Define blocks and steps as above
+#     nxblocks = (ch1.shape[1] // pix_per_cell) - cell_per_block + 1
+#     nyblocks = (ch1.shape[0] // pix_per_cell) - cell_per_block + 1
+#     nfeat_per_block = orient * cell_per_block ** 2
+#
+#     # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
+#     window = 64
+#     nblocks_per_window = (window // pix_per_cell) - cell_per_block + 1
+#     cells_per_step = 2  # Instead of overlap, define how many cells to step
+#     nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
+#     nysteps = (nyblocks - nblocks_per_window) // cells_per_step
+#
+#     # Compute individual channel HOG features for the entire image
+#     hog1 = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
+#     hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
+#     hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
+#
+#     for xb in range(nxsteps):
+#         for yb in range(nysteps):
+#             ypos = yb * cells_per_step
+#             xpos = xb * cells_per_step
+#             # Extract HOG for this patch
+#             hog_feat1 = hog1[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+#             hog_feat2 = hog2[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+#             hog_feat3 = hog3[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+#             hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3))
+#
+#             xleft = xpos * pix_per_cell
+#             ytop = ypos * pix_per_cell
+#
+#             # Extract the image patch
+#             subimg = cv2.resize(ctrans_tosearch[ytop:ytop + window, xleft:xleft + window], (64, 64))
+#
+#             # Get color features
+#             spatial_features = bin_spatial(subimg, size=spatial_size)
+#             hist_features = color_hist(subimg, nbins=hist_bins)
+#
+#             # Scale features and make a prediction
+#             test_features = X_scaler.transform(
+#                 np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))
+#             # test_features = X_scaler.transform(np.hstack((shape_feat, hist_feat)).reshape(1, -1))
+#             test_prediction = svc.predict(test_features)
+#
+#             if test_prediction == 1:
+#                 xbox_left = np.int(xleft * scale)
+#                 ytop_draw = np.int(ytop * scale)
+#                 win_draw = np.int(window * scale)
+#                 cv2.rectangle(draw_img, (xbox_left, ytop_draw + ystart),
+#                               (xbox_left + win_draw, ytop_draw + win_draw + ystart), (0, 0, 255), 6)
+#
+#     return draw_img
+#
+#
+# def apply_hog(img):
+#     """
+#
+#     :param img: The image
+#     :return:
+#     """
+#     ystart = 400
+#     ystop = 656
+#     scale = 1.5
+#
+#     out_img = hog_find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size,
+#                             hist_bins)
 
 
 
